d7527598f6c84acb29c60b7ec65c703a02f15873
MadMach420 <mach.krzsztf@gmail.com>
2022-10-17 22:30:24 +0200

    Update AnimalTest.java

--- oolab/src/test/java/agh/ics/oop/AnimalTest.java
+++ oolab/src/test/java/agh/ics/oop/AnimalTest.java
@@ -11,5 +11,17 @@ public class AnimalTest {
         assert Animal.runSeriesOfCommands(new String[]{"f", "f", "f"})
                 .toString()
                 .equals("Position: (2, 4), direction: Północ");
+        assert Animal.runSeriesOfCommands(new String[]{"b", "b", "r", "r", "f"})
+                .toString()
+                .equals("Position: (2, 0), direction: Południe");
+        assert Animal.runSeriesOfCommands(new String[]{"invalid argument 1", "invalid argument 2"})
+                .toString()
+                .equals("Position: (2, 2), direction: Północ");
+        assert Animal.runSeriesOfCommands(new String[]{"l", "invalid argument", "b"})
+                .toString()
+                .equals("Position: (3, 2), direction: Zachód");
+        assert Animal.runSeriesOfCommands(new String[]{"l", "invalid argument", "f", "f", "f", "r", "b", "b", "r"})
+                .toString()
+                .equals("Position: (0, 0), direction: Wschód");
     }
 }
